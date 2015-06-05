9baa768606b8aa5857d8e1cf77845521c8356e9f
BMK <bmktuwien@gmail.com>
2015-06-05 14:53:09 +0200

    Handle the json-rpc version correctly

--- haskell/json-server/Data/Network/JsonRpcServer.hs
+++ haskell/json-server/Data/Network/JsonRpcServer.hs
@@ -10,34 +10,44 @@ import qualified Data.Text        as T
 
 data JsonRpcRequest = JsonRpcRequest
   { jrReqId     :: !Value
+  , jrReqVer    :: !Value
   , jrReqMethod :: !T.Text
   , jrReqParams :: !Value
   } deriving (Show)
 
 data JsonRpcResponse r =
     JsonRpcResult { jrResId     :: !Value
+                  , jrResVer    :: !Value
                   , jrResResult :: r
                   }
   | JsonRpcError  { jrErrId      :: !Value
+                  , jrErrVer     :: !Value
                   , jrErrCode    :: !Int
                   , jrErrMessage :: !T.Text
                   , jrErrData    :: !Value
                   }
   deriving (Show)
 
+isValidJsonRpcVer :: Value -> Bool
+isValidJsonRpcVer (String "2.0") = True
+isValidJsonRpcVer (Null)         = True
+isValidJsonRpcVer _              = False
+
 instance FromJSON JsonRpcRequest where
   parseJSON (Object v) = do
-    String "2.0" <- v .:  "jsonrpc"
-    jid          <- v .:? "id" .!= Null -- if null it's a notification
-    method       <- v .:  "method"
-    params       <- v .:? "params" .!= emptyObject
+    jver    <- v .:? "jsonrpc" .!= Null -- if null it's jsonrpc 1.0
+    jid     <- v .:? "id" .!= Null      -- if null it's a notification
+    method  <- v .:  "method"
+    params  <- v .:? "params" .!= emptyObject
+
+    unless (isValidJsonRpcVer jver) mzero
 
     case params of
       (Array _)  -> return ()
       (Object _) -> return ()
       _          -> mzero
 
-    return $ JsonRpcRequest jid method params
+    return $ JsonRpcRequest jid jver method params
 
   parseJSON _ = mzero
 
@@ -45,36 +55,42 @@ instance (FromJSON r) => FromJSON (JsonRpcResponse r) where
   parseJSON (Object v) = parseResult `mplus` parseError
     where
       parseResult = do
-        String "2.0"  <- v .:  "jsonrpc"
-        jid           <- v .: "id"
-        result        <- v .: "result"
-        return $ JsonRpcResult jid result
+        jver   <- v .:? "jsonrpc" .!= Null
+        jid    <- v .: "id"
+        result <- v .: "result"
+
+        unless (isValidJsonRpcVer jver) mzero
+
+        return $ JsonRpcResult jid jver result
 
       parseError = do
-        String "2.0" <- v .:  "jsonrpc"
+        jver         <- v .:? "jsonrpc" .!= Null
         jid          <- v .: "id"
         errorObj     <- v .: "error"
         errorCode    <- errorObj .: "code"
         errorMessage <- errorObj .: "message"
         errorData    <- errorObj .:? "data" .!= Null
-        return $ JsonRpcError jid errorCode errorMessage errorData
+
+        unless (isValidJsonRpcVer jver) mzero
+
+        return $ JsonRpcError jid jver errorCode errorMessage errorData
 
 instance ToJSON JsonRpcRequest where
-  toJSON JsonRpcRequest{..} = object [ "jsonrpc" .= String "2.0"
+  toJSON JsonRpcRequest{..} = object [ "jsonrpc" .= jrReqVer
                                      , "id"      .= jrReqId
                                      , "method"  .= jrReqMethod
                                      , "params"  .= jrReqParams
                                      ]
 
 instance ToJSON r => ToJSON (JsonRpcResponse r) where
-  toJSON (JsonRpcResult jid result) =
-    object [ "jsonrpc" .= String "2.0"
+  toJSON (JsonRpcResult jid jver result) =
+    object [ "jsonrpc" .= jver
            , "id"      .= jid
            , "result"  .= result
            ]
 
-  toJSON (JsonRpcError jid errCode errMessage errData) =
-    object [ "jsonrpc" .= String "2.0"
+  toJSON (JsonRpcError jid jver errCode errMessage errData) =
+    object [ "jsonrpc" .= jver
            , "id"      .= jid
            , "error"   .= errObj
            ]
