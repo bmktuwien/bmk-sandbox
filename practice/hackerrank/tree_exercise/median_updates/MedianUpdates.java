61b8ddf0dc5c135b2e04e1fc3dc6fcc4707952de
BMK <bmktuwien@gmail.com>
2016-12-23 17:40:22 +0100

    Not a single thing works :(

--- practice/hackerrank/tree_exercise/median_updates/MedianUpdates.java
+++ practice/hackerrank/tree_exercise/median_updates/MedianUpdates.java
@@ -1,23 +1,89 @@
+import java.util.Scanner;
 import java.util.TreeMap;
 
 public class MedianUpdates {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
         TreeMap<Integer, Integer> map = new TreeMap<>();
 
-        int median = -1;
+        Integer median = null;
 
-        for (int i = 0; i < 10; i++) {
-            map.put(i, i);
+        for (int i = 0; i < n; i++) {
+            String s = scanner.next();
+            int v = scanner.nextInt();
 
-            if (median == -1) {
-                median = i;
+            if (median == null) {
+                if (s.equals("r")) {
+                    System.out.println("Wrong!");
+                } else {
+                    map.put(v, v);
+                    median = v;
+                    System.out.println(v);
+                }
             } else {
-                median = map.higherKey(median);
-            }
+                if (s.equals("r")) {
+                    Integer k = map.remove(v);
 
-            System.out.println(median);
+                    if (map.isEmpty()) {
+                        System.out.println("Wrong!");
+                    } else {
+                        if (k == null) {
+                            System.out.println("Wrong!");
+                        } else {
+                            // size has changed
+                            if (map.size() % 2 == 0) {
+                                if (v <= median) {
+                                    median = map.higherKey(median);
+                                }
+                            } else {
+                                if (v >= median) {
+                                    median = map.lowerKey(median);
+                                }
+                            }
+
+                            if (map.size() % 2 == 0) {
+                                int median2 = map.lowerKey(median);
+                                printMeanMedian(median2, median);
+                            } else {
+                                System.out.println(median);
+                            }
+                        }
+                    }
+
+                } else {
+                    Integer k = map.put(v, v);
+
+                    if (k == null) {
+                        // size has changed
+                        if (map.size() % 2 == 0) {
+                            if (v > median) {
+                                median = map.higherKey(median);
+                            }
+                        } else {
+                            if (v < median) {
+                                median = map.lowerKey(median);
+                            }
+                        }
+                    }
+
+                    if (map.size() % 2 == 0) {
+                        int median2 = map.lowerKey(median);
+                        printMeanMedian(median2, median);
+                    } else {
+                        System.out.println(median);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void printMeanMedian(int m1, int m2) {
+        if ((m1 + m2) % 2 == 0) {
+            System.out.println((m1 + m2) / 2);
+        } else {
+            System.out.println(String.format("%.1f", (m1 + m2) / 2.0d));
         }
     }
 }
