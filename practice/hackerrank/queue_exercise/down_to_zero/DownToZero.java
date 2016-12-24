58686e8bdac34aae410c8a1b3b0a1318a671b472
Kim Bong Min <bong-min.kim@rise-world.com>
2016-12-24 23:50:35 +0100

    Not a damn thing work :(

--- practice/hackerrank/queue_exercise/down_to_zero/DownToZero.java
+++ practice/hackerrank/queue_exercise/down_to_zero/DownToZero.java
@@ -1,6 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by bmk on 12/24/16.
@@ -9,36 +7,55 @@ public class DownToZero {
 
     public static void main(String[] args) {
         Map<Integer, Entry> factorMap = factorMap();
+        Map<Integer, Integer> minMap = minMap(factorMap);
 
         Scanner scanner = new Scanner(System.in);
         int q = scanner.nextInt();
 
         for (int i = 0; i < q; i++) {
             int n = scanner.nextInt();
-            System.out.println(minMovesToZero(n, factorMap));
+            System.out.println(minMap.get(n));
         }
     }
 
-    public static int minMovesToZero(int n, Map<Integer, Entry> factorMap) {
-        int c = 0;
+    public static Map<Integer, Integer> minMap(Map<Integer, Entry> factorMap) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 0);
+        map.put(1, 1);
 
-        while (n > 0) {
-            if (n == 1) {
-                c++;
-                break;
+        for (int i = 2; i < 1000000; i++) {
+            List<Integer> primeFactors = new ArrayList<>();
+
+            Entry e = factorMap.get(i);
+            do {
+                primeFactors.add(e.a);
+                e = factorMap.get(e.b);
+            } while (e != null);
+
+            int min = map.get(i - 1);
+
+            for (int j = 1; j < (1 << primeFactors.size()) - 1; j++) {
+                int f0 = 1;
+                int f1 = 1;
+
+                for (int k = 0; k < primeFactors.size(); k++) {
+                    if ((j >> k & 1) == 1) {
+                        f1 *= primeFactors.get(k);
+                    } else {
+                        f0 *= primeFactors.get(k);
+                    }
+                }
+
+                int t = map.get(Math.max(f0, f1));
+                if (t < min) {
+                    min = t;
+                }
             }
 
-            Entry e = factorMap.get(n);
-
-            if (Math.min(e.a, e.b) == 1) {
-                n--;
-            } else {
-                n = Math.max(e.a, e.b);
-            }
-            c++;
+            map.put(i, min + 1);
         }
 
-        return c;
+        return map;
     }
 
     public static Map<Integer, Entry> factorMap() {
