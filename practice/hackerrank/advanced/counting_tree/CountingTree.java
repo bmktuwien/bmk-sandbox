4eb9e749dbf2a75da1ef5eea339a875812325944
BMK <bmktuwien@gmail.com>
2016-12-31 15:45:04 +0100

    Nope, this crap doesn't work either

--- practice/hackerrank/advanced/counting_tree/CountingTree.java
+++ practice/hackerrank/advanced/counting_tree/CountingTree.java
@@ -56,30 +56,30 @@ public class CountingTree {
     public static List<Node> findPath(int id1, int id2) {
         Node n1 = nodes[id1];
         Node n2 = nodes[id2];
-
         List<Node> path = new ArrayList<>();
-        boolean[] seen = new boolean[100000];
 
-        Node tmp = n1;
-        while (tmp != null) {
-            seen[tmp.id] = true;
-            tmp = tmp.parent;
-        }
-
-        while (n2 != null) {
-            if (!seen[n2.id]) {
+        if (n1.height > n2.height) {
+            while (n1.height != n2.height) {
+                path.add(n1);
+                n1 = n1.parent;
+            }
+        } else if (n2.height > n1.height) {
+            while (n1.height != n2.height) {
                 path.add(n2);
                 n2 = n2.parent;
-            } else {
-                path.add(n2);
-                break;
             }
         }
 
+
         while (n1 != n2) {
             path.add(n1);
+            path.add(n2);
+
             n1 = n1.parent;
+            n2 = n2.parent;
         }
+
+        path.add(n1);
 
         return path;
     }
@@ -93,32 +93,59 @@ public class CountingTree {
         }
 
         // read edges
-        Edge[] edges = new Edge[n - 1];
         for (int i = 0; i < n - 1; i++) {
             int xId = scanner.nextInt() - 1;
             int yId = scanner.nextInt() - 1;
 
-            edges[i] = new Edge(xId, yId);
+            Node parent = nodes[xId];
+            Node child = nodes[yId];
+            child.addParent(parent);
         }
 
-        // build tree
-        for (Edge e : edges) {
-            Node parent = nodes[e.xId];
-            Node child = nodes[e.yId];
+        // add children
+        Node root = null;
+        for (Node node : nodes) {
+            if (node.parent != null) {
+                node.parent.addChild(node);
+            } else {
+                root = node;
+            }
+        }
 
-            child.addParent(parent);
+        // update height information
+        int height = 1;
+        List<Node> nextLvlNodes = new ArrayList<>(root.children);
+
+        while (!nextLvlNodes.isEmpty()) {
+            List<Node> l = new ArrayList<>();
+
+            for (Node node : nextLvlNodes) {
+                node.height = height;
+                l.addAll(node.children);
+            }
+
+            height++;
+            nextLvlNodes = l;
         }
     }
 
     public static class Node {
         final int id;
         final long data;
+        int height;
         Node parent;
+        HashSet<Node> children;
 
         public Node(int id, long data) {
             this.id = id;
             this.data = data;
             this.parent = null;
+            this.height = 0;
+            this.children = new HashSet<>();
+        }
+
+        public void addChild(Node child) {
+            this.children.add(child);
         }
 
         public void addParent(Node parent) {
@@ -142,16 +169,6 @@ public class CountingTree {
             while (parent != null) {
                 parent = parent.parent;
             }
-        }
-    }
-
-    public static class Edge {
-        final int xId;
-        final int yId;
-
-        public Edge(int xId, int yId) {
-            this.xId = xId;
-            this.yId = yId;
         }
     }
 }
